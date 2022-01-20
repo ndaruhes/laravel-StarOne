8d592d9bbe2eed200efd591dbcc31ec4dc685473
Ndaru <muhamad.ndaru@gmail.com>
2022-01-20 22:45:40 +0700

    oke

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
-        <a class="navbar-brand" href="#">StarOne</a>
+        <a class="navbar-brand" href="#"><i class="uil uil-star me-1"></i>StarOne</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
